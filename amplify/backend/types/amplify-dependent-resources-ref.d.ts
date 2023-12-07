58d3187861cd2ff965a634108a3341af90fa1616
Mohamed Abdul <mohamedyousufmo@gmail.com>
2023-12-06 23:51:02 -0800

    exam

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlbb1c37bb": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "mycoolrepo": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
+import './App.css';
+import { createNote, deleteNote } from './graphql/mutations'
+import { listNotes } from './graphql/queries'
+import { withAuthenticator, Button, Text, Flex, Heading,
   Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+  TextField,
+  View } from "@aws-amplify/ui-react";
+import { useCallback, useEffect, useState } from 'react';
+import { generateClient } from 'aws-amplify/api';
+import { Storage } from 'aws-amplify';
 
 function App({ signOut }) {
+  const client = generateClient();
+  const [notes, setNotes] = useState([])
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+    setNotes(notesFromAPI);
+  }
+
+  const handleCreateNote = useCallback(async () => {
+    await client.graphql({
+      query: createNote,
+      variables: { input: { text: window.prompt("New note") } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
+
+  const handleDeleteNote = useCallback(async (id) => {
+    await client.graphql({
+      query: deleteNote,
+      variables: { input: { id: id } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
+
+  useEffect(() => {
+    fetchNotes()
+  }, [fetchNotes])
+
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <Flex direction={"column"}>
+      <Flex justifyContent={'space-between'}>
+        <Heading level={1}>My notes</Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Flex>
+      {notes.map(note => <Flex alignItems={'center'}>
+        <Text>{note.text}</Text>
+        <Button onClick={() => handleDeleteNote(note.id)}>Remove</Button>
+      </Flex>)}
+      <Button onClick={handleCreateNote}>Add Note</Button>
+    </Flex>
   );
 }
 
